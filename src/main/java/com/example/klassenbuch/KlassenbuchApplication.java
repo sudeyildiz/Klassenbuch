86e6593788ba3273fa93fadeb32d021a245a9845
suden <yil210461@spengergasse.at>
2024-04-23 15:57:10 +0200

    Initial commit

---
+++ src/main/java/com/example/klassenbuch/KlassenbuchApplication.java
@@ -0,0 +1,13 @@
+package com.example.klassenbuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlassenbuchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlassenbuchApplication.class, args);
+    }
+
+}
